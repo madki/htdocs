df652ec0f99c5b2b001ef2e647989792e12b0717
Madhu kiran <p.madhu.kiran96@gmail.com>
2014-05-24 19:56:53 +0530

    first commit

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+madhukiran:$1$0XudMV8j$0ra5dG.fD3hALUWJlNSzf1:Madhu kiran:p.madhu.kiran96@gmail.com:admin,user
